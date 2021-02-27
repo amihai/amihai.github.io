ac9a58dfebc8ee520f7c1818004c973f73a24661
Andrei MIHAI <user.andrei@yahoo.com>
2021-02-27 20:05:38 +0200

    Set dimensions of the canvas

--- static/totolici.js
+++ static/totolici.js
@@ -1,8 +1,8 @@
 $(function(){
 	const width  = $(window).width();
     const height = $(window).height();
-	const canvasWidth =  600; //width * 0.8;
-	const canvasHeight = 150; //height * 0.8;
+	const canvasWidth =  800; //width * 0.8;
+	const canvasHeight = 400; //height * 0.8;
 	
 	var canvas = $("#game-canvas");
 	ctx = canvas[0].getContext('2d');
