32ca58df63b7db5b1f65f4ccf91ca947a8634c89
Andrei MIHAI <user.andrei@yahoo.com>
2021-02-28 15:11:01 +0200

    Deploy last version done by Andreea

--- static/totolici.js
+++ static/totolici.js
@@ -14,10 +14,12 @@ $(function(){
 	var player1InitialLocationH = horizontalLine - player1Height;
 	var player1LocationH = player1InitialLocationH;
 	var environmentPosition = 0;
+	var lives = 5;
 	
 	var player1_1 = document.getElementById("player1-1");
 	var player1_2 = document.getElementById("player1-2");
 	var player1_jump = document.getElementById("player1-jump");
+	var player1_dead = document.getElementById("player1-dead");
 	var soil = document.getElementById("soil");
 	var portal1 = document.getElementById("portal-1");
 	var portal2 = document.getElementById("portal-2");
@@ -64,7 +66,48 @@ $(function(){
 	var up = false;
 	var down = false;
 	
+	var lastObstacoleHit = -1;
+	function isHit() {
+		var bottomLeftCornerW = player1LocationW + player1Width;
+		var bottomLeftCornerH = player1LocationH + player1Height;
+		for (var indexObstacle = firstObstacleIndex; indexObstacle < firstObstacleIndex + obstaclesPerScreen; indexObstacle++) {
+			if (lastObstacoleHit == indexObstacle) {
+				continue;
+			}
+			var obstacle = obstacles[indexObstacle];
+			var obstacleStarting = indexObstacle * 200 - environmentPosition;
+			if (obstacle) {
+				var obstacleStartW = obstacleStarting;
+				var obstacleStartH = horizontalLine - obstacle.height;
+				var obstacleEndW = obstacleStartW + obstacle.width;
+				var obstacleEndH = obstacleStartH + obstacle.height;
+				if (bottomLeftCornerW >= obstacleStartW && bottomLeftCornerW <= obstacleEndW
+					&& bottomLeftCornerH >= obstacleEndH && bottomLeftCornerH <= obstacleEndH) {
+						lives = lives - 1; 
+						lastObstacoleHit = indexObstacle;
+					}
+			}
+		}
+	}
 	
+	
+	var isDeadImage = false;
+	function drawGameOver() {
+		ctx.font = "100px Regular semi-serif";
+		ctx.fillStyle = "#DF1313";
+		ctx.fillText("GAME OVER", 100, 150);
+		
+		const currentDate = new Date();
+		const timestamp = currentDate.getTime();
+		if (timestamp % 10 == 0) {
+			isDeadImage = !isDeadImage;
+		}
+		if (isDeadImage) {
+			ctx.drawImage(player1_dead, player1LocationW, player1LocationH, player1Width, player1Height);
+		} else {
+			ctx.drawImage(player1Image, player1LocationW, player1LocationH, player1Width, player1Height);
+		}
+	}
 	function drawEnvironment() {
 		ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 		// draw horizon
@@ -80,16 +123,26 @@ $(function(){
 				obstacles.push(getRandomObstacle());
 			}
 			if (obstacle) {
-				ctx.drawImage(obstacle, obstacleStarting, horizontalLine - obstacle.height, obstacle.width, obstacle.height);
-				
+				ctx.drawImage(obstacle, obstacleStarting, horizontalLine - obstacle.height, obstacle.width, obstacle.height);	
 			}
 		}
 		ctx.font = "14px Regular semi-serif";
 		ctx.fillText("SCORE: " + Math.floor(environmentPosition/100), 600, 10);
-		ctx.fillText(playerName, 10, 10);
+		//ctx.fillText(playerName, 10, 10);
+		
+		isHit();
+		
+		for (var i=0; i < lives; i++) {
+			ctx.drawImage(heart, 10 + i * 20, 10, 13, 10);
+		}
+		
 	}
 	
 	window.setInterval(function () {
+		if (lives == 0) {
+			drawGameOver();
+			return;
+		}
 		environmentPosition = environmentPosition+1;
 		
 		var jump = up || down;
