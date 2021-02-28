1b7cf9a2dcbb48343b3803e346fa8060a1d423a3
Andrei MIHAI <user.andrei@yahoo.com>
2021-02-28 02:00:19 +0200

    Add portal obstacles

--- static/totolici.js
+++ static/totolici.js
@@ -1,3 +1,4 @@
+
 $(function(){
 	const canvasWidth =  800;
 	const canvasHeight = 400;
@@ -17,6 +18,33 @@ $(function(){
 	var player1_2 = document.getElementById("player1-2");
 	var player1_jump = document.getElementById("player1-jump");
 	var soil = document.getElementById("soil");
+	var portal1 = document.getElementById("portal-1");
+	var portal2 = document.getElementById("portal-2");
+	var portal3 = document.getElementById("portal-3");
+	var portal4 = document.getElementById("portal-4");
+	var firstObstacleIndex = 0;
+	var obstaclesPerScreen = 4;
+	var obstacles = []; 
+	
+	function getRandomObstacle() {
+		var rand = Math.floor((Math.random() * 10) + 1);
+		if (rand == 1) {
+			return portal1;
+		} else if (rand == 2) {
+			return portal2;
+		} else if (rand == 3) {
+			return portal3;
+		} else if (rand == 4) {
+			return portal4;
+		} else {
+			return undefined;
+		}
+	}
+	
+	// generate initial list of obstacles
+	for (var i = 0; i < 10; i++) {
+		obstacles.push(getRandomObstacle());
+	}
 	
 	var canvas = $("#game-canvas");
 	ctx = canvas[0].getContext('2d');
@@ -35,12 +63,31 @@ $(function(){
 	var up = false;
 	var down = false;
 	
+	
 	function drawEnvironment() {
 		ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 		// draw horizon
 		var soilStarting = environmentPosition % canvasWidth; 
 		ctx.drawImage(soil, -soilStarting, horizontalLine + 1, canvasWidth, canvasHeight - horizontalLine);
 		ctx.drawImage(soil, canvasWidth-soilStarting, horizontalLine + 1, canvasWidth, canvasHeight - horizontalLine);
+		
+		// add a new obstacle in the list
+		//if (obstacleStarting == 100) {
+		//	currentObstacleIndex++;
+		//	console.log(currentObstacleIndex);
+		//}
+		for (var indexObstacle = firstObstacleIndex; indexObstacle < firstObstacleIndex + obstaclesPerScreen; indexObstacle++) {
+			var obstacle = obstacles[indexObstacle];
+			var obstacleStarting = indexObstacle * 200 - environmentPosition;
+			if (obstacleStarting < 0) {
+				firstObstacleIndex++;
+				obstacles.push(getRandomObstacle());
+			}
+			if (obstacle) {
+				ctx.drawImage(obstacle, obstacleStarting, horizontalLine - obstacle.height, obstacle.width, obstacle.height);
+				
+			}
+		}
 	}
 	
 	window.setInterval(function () {
